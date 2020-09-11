f804b64ff34e992b3d4b172d09f84128104ad545
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2020-09-11 14:23:16 -0300

    Tabela de usuários

--- web/ceres-growth-dashboard/src/App.tsx
+++ web/ceres-growth-dashboard/src/App.tsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MaterialTable, { Column } from 'material-table';
+import userContent from './userContent.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Row {
+  username?: string | null;
+  full_name?: string | null;
+  biography?: string | null;
+  id?: string | null;
+  business_email?: string | null;
+  connected_fb_page?: string | null;
+  is_business_account?: boolean | null;
+  is_private?: boolean | null;
+  is_verified?: boolean | null;
+  success?: boolean | null;
+  external_url?: string | null;
 }
 
-export default App;
+interface TableState {
+  columns: Array<Column<Row>>;
+  data: Row[];
+}
+
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState<TableState>({
+    columns: [
+      { title: 'username', field: 'username' },
+      { title: 'full_name', field: 'full_name' },
+      { title: 'biography', field: 'biography' },
+      { title: 'id', field: 'id' },
+      { title: 'business_email', field: 'business_email' },
+      { title: 'connected_fb_page', field: 'connected_fb_page' },
+      { title: 'is_business_account', field: 'is_business_account' },
+      { title: 'is_private', field: 'is_private' },
+      { title: 'is_verified', field: 'is_verified' },
+      { title: 'success', field: 'success' },
+      { title: 'external_url', field: 'external_url' },
+    ],
+    data: userContent,
+  });
+
+  return (
+    <MaterialTable
+      title="Editable Example"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}
