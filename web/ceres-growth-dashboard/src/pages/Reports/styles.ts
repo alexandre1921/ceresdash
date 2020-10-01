285e9fdc367b3c6a405a648c25f95c7568572076
ricardodesouza1 <ricardo270296souza@gmail.com>
2020-10-01 20:29:02 -0300

    Correção select e gráficos

--- web/ceres-growth-dashboard/src/pages/Reports/styles.ts
+++ web/ceres-growth-dashboard/src/pages/Reports/styles.ts
@@ -1,25 +1,5 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  /* Lista de usuários */
-
-  position: absolute;
-  width: 294px;
-  height: 39px;
-  left: 331px;
-  top: 45px;
-
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 32px;
-  line-height: 39px;
-
-  /* identical to box height */
-
-  color: #0d4137;
-`;
-
 export const Grafico = styled.div`
   position: absolute;
   width: 75.3%;
