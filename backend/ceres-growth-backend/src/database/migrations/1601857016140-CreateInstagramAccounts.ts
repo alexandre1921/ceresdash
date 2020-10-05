f800c2d45e71d1e0ff910e93cfae5a8803dd54de
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2020-10-05 11:41:19 -0300

    migration instagram users

---
+++ backend/ceres-growth-backend/src/database/migrations/1601857016140-CreateInstagramAccounts.ts
@@ -0,0 +1,91 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateInstagramAccounts1601857016140
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'instagram_accounts',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isNullable: false,
+            length: '30',
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            isNullable: false,
+            length: '50',
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+            length: '50',
+          },
+          {
+            name: 'biography',
+            type: 'varchar',
+            isNullable: false,
+            length: '150',
+          },
+          {
+            name: 'connected_fb_page',
+            type: 'varchar',
+            isNullable: false,
+            length: '1200',
+          },
+          {
+            name: 'external_url',
+            type: 'varchar',
+            isNullable: false,
+            length: '1200',
+          },
+          {
+            name: 'business_email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+            length: '255',
+          },
+          {
+            name: 'is_business_account',
+            type: 'boolean',
+            isNullable: false,
+          },
+          {
+            name: 'is_private',
+            type: 'boolean',
+            isNullable: false,
+          },
+          {
+            name: 'is_verified',
+            type: 'boolean',
+            isNullable: false,
+          },
+          {
+            name: 'success',
+            type: 'boolean',
+            isNullable: false,
+          },
+          {
+            name: 'created_on',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+          {
+            name: 'updated_on',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('instagram_accounts');
+  }
+}
