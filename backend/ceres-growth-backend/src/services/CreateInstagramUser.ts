ff183a4bb37797f003971522031f67a5c486aca7
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2020-10-08 17:37:25 -0300

    Retornando lista de followers

--- backend/ceres-growth-backend/src/services/CreateInstagramUser.ts
+++ backend/ceres-growth-backend/src/services/CreateInstagramUser.ts
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import { getCustomRepository } from 'typeorm';
 import fetch, { Response } from 'node-fetch';
+import { existsSync } from 'fs';
 import FormData = require('form-data');
 import CreateSessionInSocialMedia from './CreateSessionInSocialMedia';
 
@@ -8,6 +9,7 @@ import AppError from '../errors/AppError';
 
 import InstagramUser from '../models/InstagramUser';
 import InstagramUserRepository from '../repositories/IntagramUsersRepository';
+import CreateJson from './CreateJson';
 
 interface Request {
   username: string | undefined;
@@ -106,24 +108,80 @@ class CreateInstagramUser {
     //     };
     //   });
 
-    const createEncPassword = (pwd: string) => {
-      return `#PWD_INSTAGRAM_BROWSER:0:${Date.now()}:${pwd}`;
-    };
+    let sessionId = '';
+    const sessionIdFilePath = `${__dirname}/../json/sessionId.json`;
+    if (!existsSync(sessionIdFilePath)) {
+      const createEncPassword = (pwd: string) => {
+        return `#PWD_INSTAGRAM_BROWSER:0:${Date.now()}:${pwd}`;
+      };
 
-    const formData = new FormData();
-    formData.append('username', 'mohammed_kibe');
-    formData.append('enc_password', createEncPassword('Inteligente01'));
+      const formData = new FormData();
+      formData.append('username', 'mohammed_kibe');
+      formData.append('enc_password', createEncPassword('Inteligente01'));
 
-    const login = await fetch(
-      `https://www.instagram.com/accounts/login/ajax/`,
-      {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'X-CSRFToken': 'csrftoken',
+      const login = await fetch(
+        `https://www.instagram.com/accounts/login/ajax/`,
+        {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'X-CSRFToken': 'csrftoken',
+          },
         },
-      },
-    ).then(res => console.log(res.headers));
+      ).then(res => res.headers.get('set-cookie')); // res.headers.get('set-cookie')
+
+      sessionId = `sessionid=${
+        login?.toString().split('sessionid=')[1].split(';')[0]
+      }`;
+
+      console.log('Criando Login');
+      CreateJson(sessionIdFilePath, sessionId, 'sessionId criado com sucesso');
+    } else {
+      console.log('Carregando Login');
+      sessionId = require(sessionIdFilePath);
+    }
+
+    try {
+      let followers: any[] = [];
+      let after = null;
+      let has_next = true;
+      while (has_next) {
+        // eslint-disable-next-line no-await-in-loop
+        await fetch(
+          `https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=${encodeURIComponent(
+            JSON.stringify({
+              id: '8064976408',
+              include_reel: true,
+              fetch_mutual: true,
+              first: 50,
+              after,
+            }),
+          )}`,
+          {
+            headers: {
+              cookie: sessionId,
+            },
+          },
+        )
+          // eslint-disable-next-line no-loop-func
+          .then(res1 => res1.json())
+          // eslint-disable-next-line no-loop-func
+          .then(res2 => {
+            has_next = res2.data.user.edge_followed_by.page_info.has_next_page;
+            after = res2.data.user.edge_followed_by.page_info.end_cursor;
+            followers = followers.concat(
+              res2.data.user.edge_followed_by.edges.map(
+                ({ node }: { node: { username: string } }) => {
+                  return node.username;
+                },
+              ),
+            );
+          });
+      }
+      console.log({ followers, success: true });
+    } catch (err) {
+      console.log('Invalid username');
+    }
 
     return true;
   }
