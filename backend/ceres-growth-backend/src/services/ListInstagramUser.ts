56c15d19dfa54e47fda1328ebeaee66e897d1c65
Alexandre1921 <alexandre_leonardo2010@hotmail.com>
2020-11-22 18:25:05 -0300

    Take funcionando

--- backend/ceres-growth-backend/src/services/ListInstagramUser.ts
+++ backend/ceres-growth-backend/src/services/ListInstagramUser.ts
@@ -5,17 +5,24 @@ import InstagramUserRepository from '../repositories/IntagramUsersRepository';
 
 interface Request {
   username: string;
-  take:number;
-  filter:string;
+  take: number;
+  filter: string;
 }
 
 class CreateInstagramUser {
-  public async execute({username,take,filter}:Request): Promise<InstagramUser[]> {
+  public async execute({
+    username,
+    take,
+    filter,
+  }: Request): Promise<InstagramUser[]> {
     const instagramUserRepository = getCustomRepository(
       InstagramUserRepository,
     );
 
-    const instagramUsers = instagramUserRepository.find(username?{where:{username:Like(`%${username}%`)}}:{take});
+    const instagramUsers = instagramUserRepository.find({
+      take,
+      where: { username: Like(`%${username}%`) },
+    });
 
     return instagramUsers;
   }
